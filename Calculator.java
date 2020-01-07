6795be3e47134630111ba08664dd14dabbd2d188
last <z.last.zlo@gmail.com>
2020-01-07 22:44:09 +0200

    user input

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+/**
+	
+	Класс реализует калькулятор
+*/
+
+public class Calculator {
+	/**
+	результат вычисления
+	*/
+	private int result;
+	/**
+	* Суммирует аргументы
+	* @param params аргументы суммирования
+	*/
+	public void add(int ... params) {
+		for(Integer param : params) {
+			this.result+= param;
+		}
+	}
+	/**
+	*	Получить результат
+	*	@return результат вычисления
+	*/
+	public int getResult() {
+		return this.result;
+	}
+	/**
+	* Очистить результат вычисления
+	*/
+	public void cleanResult(){
+		this.result = 0;
+	}
+}
